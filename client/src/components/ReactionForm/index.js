dfc1d30bab51f25a79711b437683e2a25a284228
Marcus0224 <marcstlouis04@gmail.com>
2022-08-23 20:52:43 -0400

    reaction form update

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -1,9 +1,12 @@
+import { useMutation } from "@apollo/client";
 import React, { useState} from "react";
+import { ADD_REACTION } from "../../utils/mutations";
 
 
 const ReactionForm = ({ thoughtId }) => {
     const [ reactionBody, setBody] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
+    const [ addReaction, { error }] = useMutation(ADD_REACTION);
 
     const handleChange = event => {
         if (event.target.value.length <= 200) {
@@ -12,20 +15,37 @@ const ReactionForm = ({ thoughtId }) => {
         }
     };
 
-    const handleFormSubmit = async eevnt => {
+    const handleFormSubmit = async event => {
         event.preventDefault();
+
+        try {
+            //add thought to database
+            await addReaction({
+                variables: { reactionBody, thoughtId }
+            });
+            //clear from value
+        
         setBody('');
         setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+        }
     };
+    
     return (
         <div>
-            <p className="m-0">
-                Character Count: 0/200
+            <p className={`m-0 ${characterCount === 200 ? 'text-error' : ''}`}>
+                Character Count: {characterCount}/200
+                {error && <span className="ml-2">Something went wrong...</span>}
             </p>
-            <form className="flex-row justify-center justify-space-between-md align-stretch">
+            <form className="flex-row justify-center justify-space-between-md align-stretch"
+                onSubmit={handleFormSubmit}
+                >
                 <textarea
                 placeholder="Leave a reaction to this thought..."
+                value={thoughtText}
                 className="form-input col-12 col-md-9"
+                onChange={handleChange}
                 ></textarea>
 
                 <button className="btn col-12 col-md-3" type="submit">
